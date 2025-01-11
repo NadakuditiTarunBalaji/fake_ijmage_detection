828ca5c2f731adf5f7106fbc0d47f7599ecddc2b
NadakuditiTarunBalaji <nadakudititarunbalaji@gmail.com>
2025-01-11 16:55:11 +0530

    -a

--- model2.py
+++ model2.py
@@ -113,7 +113,3 @@ plt.grid(True)
 plt.legend(['Train', 'Val'])
 
 plt.show()
-
-# To load the saved model later:
-# from tensorflow.keras.models import load_model
-# loaded_model = load_model('face_detection_model.keras')
